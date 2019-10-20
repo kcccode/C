791cac29cb460f86987959305df6637002716c97
chenkocheng <kochengc@andrew.cmu.edu>
2019-10-20 18:43:16 -0400

    init linklist

---
+++ linklist.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+  int value;
+  int *next;
+} list;
+
+typedef struct {
+  list *head;
+  list *end;
+} queue;
+
+int main(int argc, char argv) {
+
+  return 0;
+}
