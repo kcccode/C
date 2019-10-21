9a569b8329d5d63e4d61d4ecf7844bfc60d42f46
chenkocheng <kochengc@andrew.cmu.edu>
2019-10-20 21:16:58 -0400

    complete the example

--- linklist.c
+++ linklist.c
@@ -1,17 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct {
+typedef struct ele{
   int value;
-  int *next;
-} list;
+  struct ele *next;
+} list_ele;
 
 typedef struct {
-  list *head;
-  list *end;
+  list_ele *head;
+  list_ele *end;
 } queue;
 
 int main(int argc, char argv) {
+  
+  list_ele A, B;
+  list_ele *ptr = &A;
 
+  A.value = 2;
+  A.next = &B;
+  B.value = 4;
+  B.next = NULL;
+
+  printf("*ptr: %p\n", ptr);
+  printf("A.value: %d\n", A.value);
+  printf("A.next: %p\n", A.next);
+  printf("B.value: %d\n", B.value);
+  printf("B.next: %p\n", B.next);
+  
   return 0;
 }
